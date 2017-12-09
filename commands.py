711d07a012aa3496d2566ef2c1170316b77f7bfd
paulamanz <pamaiz@gmail.com>
2017-12-09 20:18:32 +0100

    Updated Openshift Info

--- commands.py
+++ commands.py
@@ -6,8 +6,9 @@ def sayHello(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Hola! Bienvenidos a la charla de OpenShift")
 
 def info(bot, update):
-
-    bot.send_message(chat_id=update.message.chat_id, text="Informacion de openshift.")
+    mytext = "Red Hat OpenShift Online es el servicio público de alojamiento de la plataforma como servicio (PaaS) de Red Hat que ofrece una solución de desarrollo, creación, implementación y alojamiento de aplicaciones en nube."
+    
+    bot.send_message(chat_id=update.message.chat_id, text=mytext)
 
 def photo (bot, update):
     bot.send_photo(chat_id=update.message.chat_id, photo=constants.AOS_IMAGES[randint(0, constants.AOS_IMAGES.__len__()-1)])
